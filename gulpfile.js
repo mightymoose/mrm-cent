b11ca75c7a2c5516b3b23b54f2997446a77b817a
Ryan Morris <mryan.morris@gmail.com>
2014-02-28 22:49:57 -0600

    Set up travis.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 var gulp = require('gulp');
 var karma = require('gulp-karma');
 
-gulp.task('default', function(){
-  var testFiles = [
-    'lib/angular/angular.js',
-    'lib/angular-mocks/angular-mocks.js',
-    '*.test.js'
-  ]
+var testFiles = [
+  'lib/angular/angular.js',
+  'lib/angular-mocks/angular-mocks.js',
+  '*.test.js'
+];
 
+gulp.task('test', function(){
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }));
+});
+
+gulp.task('default', function(){
   return gulp.src(testFiles)
     .pipe(karma({
       configFile: 'karma.conf.js',
       action: 'watch'
-    }))
+    }));
 });
